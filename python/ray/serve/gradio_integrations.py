a7d8bf30518721c311d86257f4a7a2378586afe2
Cindy Zhang <cindyzyx9@gmail.com>
2022-08-25 12:50:44 -0700

    add ray manager
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/gradio_integrations.py
+++ python/ray/serve/gradio_integrations.py
@@ -1,8 +1,10 @@
 from ray import serve
 from ray.serve._private.http_util import ASGIHTTPSender
 from ray.util.annotations import PublicAPI
+from ray.serve.drivers import DAGDriver
 
 import starlette
+from typing import Callable
 
 try:
     import gradio as gr
@@ -25,3 +27,57 @@ class GradioIngress:
 
 
 GradioServer = serve.deployment(GradioIngress)
+
+
+@PublicAPI(stability="alpha")
+class RayManager:
+    """Context Manager that allows users to independently scale out Gradio interfaces.
+
+    Example usage:
+        with RayManager() as rm:
+            demo = gr.Parallel(
+                gr.Interface(rm.rayify(fn1), "text", "text"),
+                gr.Interface(rm.rayify(fn2), "text", "text"),
+            )
+
+            rm.launch_ray_backend()
+            demo.launch()
+    """
+
+    def __init__(self):
+        self.route_to_node = {}
+
+    def __enter__(self):
+        return self
+
+    def rayify(self, fn: Callable, name: str = None, num_replicas: int = 1) -> Callable:
+        """Scales out a function.
+
+        When launch_ray_backend() is called, deploys the function as a Serve deployment.
+        """
+        if name is None:
+            name = fn.__name__
+        route = f"/{name}"
+
+        @serve.deployment(name=name, num_replicas=num_replicas)
+        class Model:
+            def __init__(self, fn):
+                self.fn = fn
+
+            async def __call__(self, *args):
+                return self.fn(*args)
+
+        self.route_to_node[route] = Model.bind(fn)
+
+        async def f(*args):
+            return await self.handle.predict_with_route.remote(route, *args)
+
+        return f
+
+    def launch_ray_backend(self):
+        """Deploys the functions processed through rayify()"""
+        app = DAGDriver.bind(self.route_to_node)
+        self.handle = serve.run(app)
+
+    def __exit__(self, *args):
+        serve.shutdown()
--- python/ray/serve/tests/test_gradio.py
+++ python/ray/serve/tests/test_gradio.py
@@ -1,6 +1,6 @@
 import ray
 from ray import serve
-from ray.serve.gradio_integrations import GradioServer
+from ray.serve.gradio_integrations import GradioServer, RayManager
 
 import gradio as gr
 
@@ -64,6 +64,67 @@ def test_gradio_ingress_scaling(serve_start_shutdown):
         pids.append(response.json()["data"][0])
 
     assert len(set(pids)) == 2
+
+
+def test_gradio_parallel_correctness(serve_start_shutdown):
+    """
+    Ensure a Gradio Parallel interface scaled out through RayManager still produces
+    correct output.
+    """
+
+    def greet(name):
+        return f"Good morning {name}!"
+
+    def goodbye(name):
+        return f"See you later {name}!"
+
+    with RayManager() as rm:
+        io = gr.Parallel(
+            gr.Interface(rm.rayify(greet), "text", "text"),
+            gr.Interface(rm.rayify(goodbye), "text", "text"),
+        )
+
+        rm.launch_ray_backend()
+        (_, url, _) = io.launch(prevent_thread_lock=True)
+
+        test_input = "Alice"
+        response = requests.post(
+            f"{url.strip('/')}/api/predict/", json={"data": [test_input]}
+        )
+        print(response.json())
+        assert response.status_code == 200 and response.json()["data"] == [
+            greet(test_input),
+            goodbye(test_input),
+        ]
+
+
+def test_gradio_parallel_scaling(serve_start_shutdown):
+    """
+    Check that a Gradio Parallel app that has been scaled out through RayManager 
+    actually scales, i.e. separate client requests are served by different replicas.
+    """
+
+    def f(*args):
+        return os.getpid()
+
+    with RayManager() as rm:
+        io = gr.Parallel(
+            gr.Interface(rm.rayify(f, name='f_1', num_replicas=2), "text", "text"),
+            gr.Interface(rm.rayify(f, name='f_2', num_replicas=2), "text", "text"),
+        )
+
+        rm.launch_ray_backend()
+        (_, url, _) = io.launch(prevent_thread_lock=True)
+
+        pids = []
+        for _ in range(3):
+            response = requests.post(
+                f"{url.strip('/')}/api/predict/", json={"data": ["input"]}
+            )
+            assert response.status_code == 200
+            pids.extend(response.json()["data"])
+
+        assert len(set(pids)) == 4
 
 
 if __name__ == "__main__":
