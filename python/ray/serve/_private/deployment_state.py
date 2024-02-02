ecf95434e79acc3ae48762a31f9a0a3e2a3815ad
Cindy Zhang <cindyzyx9@gmail.com>
2024-02-02 09:20:46 -0800

    [serve] autoscaling release tests
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/deployment_state.py
+++ python/ray/serve/_private/deployment_state.py
@@ -1654,7 +1654,7 @@ class DeploymentState:
             return
 
         logger.info(
-            f"Autoscaling replicas for deployment '{self.deployment_name}' in "
+            f"Autoscalingggg replicas for deployment '{self.deployment_name}' in "
             f"application '{self.app_name}' to {decision_num_replicas}. "
             f"Current number of requests: {total_num_requests}. Current number of "
             f"running replicas: {num_running_replicas}."
