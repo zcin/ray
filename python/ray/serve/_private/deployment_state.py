dd6f88c4dcf95edcc0dfd933ac763874bb7fa14e
Cindy Zhang <cindyzyx9@gmail.com>
2024-03-01 06:46:02 -0800

    [serve] compact nodes
    Add logic for compacting nodes. Calls `detect_compact_opportunities` if Serve cluster is in steady state.
    
    
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/deployment_state.py
+++ python/ray/serve/_private/deployment_state.py
@@ -38,6 +38,7 @@ from ray.serve._private.constants import (
     RAY_SERVE_COLLECT_AUTOSCALING_METRICS_ON_HANDLE,
     RAY_SERVE_EAGERLY_START_REPLACEMENT_REPLICAS,
     RAY_SERVE_FORCE_STOP_UNHEALTHY_REPLICAS,
+    RAY_SERVE_USE_COMPACT_SCHEDULING_STRATEGY,
     REPLICA_HEALTH_CHECK_UNHEALTHY_THRESHOLD,
     SERVE_LOGGER_NAME,
     SERVE_NAMESPACE,
@@ -1385,6 +1386,11 @@ class DeploymentState:
             )
         ]
 
+    def get_num_running_at_target_version(self) -> int:
+        return self._replicas.count(
+            states=[ReplicaState.RUNNING], version=self._target_state.version
+        )
+
     def get_active_node_ids(self) -> Set[str]:
         """Get the node ids of all running replicas in this deployment.
 
@@ -2317,8 +2323,13 @@ class DeploymentState:
 
         return to_stop, remaining
 
-    def _migrate_replicas_on_draining_nodes(self):
+    def _migrate_replicas_on_draining_nodes(self, in_steady_state: bool):
         draining_node_deadlines = self._cluster_node_info_cache.get_draining_nodes()
+        if RAY_SERVE_USE_COMPACT_SCHEDULING_STRATEGY and in_steady_state:
+            node, deadline = self._deployment_scheduler.detect_compact_opportunities()
+            if node:
+                draining_node_deadlines[node] = deadline
+
         for replica in self._replicas.pop(
             states=[ReplicaState.UPDATING, ReplicaState.RUNNING, ReplicaState.STARTING]
         ):
@@ -2361,7 +2372,7 @@ class DeploymentState:
         for replica in replicas_to_keep:
             self._replicas.add(ReplicaState.PENDING_MIGRATION, replica)
 
-    def update(self) -> DeploymentStateUpdateResult:
+    def update(self, in_steady_state: bool) -> DeploymentStateUpdateResult:
         """Attempts to reconcile this deployment to match its goal state.
 
         This is an asynchronous call; it's expected to be called repeatedly.
@@ -2381,7 +2392,7 @@ class DeploymentState:
             # Check the state of existing replicas and transition if necessary.
             self._check_and_update_replicas()
 
-            self._migrate_replicas_on_draining_nodes()
+            self._migrate_replicas_on_draining_nodes(in_steady_state)
 
             upscale, downscale = self._scale_deployment_replicas()
 
@@ -2805,11 +2816,20 @@ class DeploymentStateManager:
         upscales = {}
         downscales = {}
 
+        # NOTE(zcin): If the deployment status is healthy, it should mean
+        # that the number of running replicas at target version is at
+        # the target number. Adding an extra check however to be defensive.
+        in_steady_state = all(
+            ds.curr_status_info.status == DeploymentStatus.HEALTHY
+            and ds.get_num_running_at_target_version()
+            == ds._target_state.target_num_replicas
+            for ds in self._deployment_states.values()
+        )
         for deployment_id, deployment_state in self._deployment_states.items():
             if deployment_state.should_autoscale():
                 deployment_state.autoscale()
 
-            deployment_state_update_result = deployment_state.update()
+            deployment_state_update_result = deployment_state.update(in_steady_state)
             if deployment_state_update_result.upscale:
                 upscales[deployment_id] = deployment_state_update_result.upscale
             if deployment_state_update_result.downscale:
