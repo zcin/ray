a837f3fb96bb4526d8c140fbb640f05be1869563
Cindy Zhang <cindyzyx9@gmail.com>
2024-04-05 10:00:01 -0700

    [serve] Drop handle metrics from dead actors
    When Serve replica actors die or HTTP proxy actors die, drop metrics from all handles that live on those actors. This doesn't cover everything (e.g. handles that live in driver scripts, or tasks, or any non-serve actors), but it should cover most cases, and the remaining cases will be handled by the default 20s timeout invalidation.
    
    
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/controller.py
+++ python/ray/serve/_private/controller.py
@@ -153,11 +153,11 @@ class ServeController:
         self.cluster_node_info_cache.update()
 
         self.proxy_state_manager = ProxyStateManager(
-            http_config,
-            self._controller_node_id,
-            self.cluster_node_info_cache,
-            self.global_logging_config,
-            grpc_options,
+            config=http_config,
+            head_node_id=self._controller_node_id,
+            cluster_node_info_cache=self.cluster_node_info_cache,
+            logging_config=self.global_logging_config,
+            grpc_options=grpc_options,
         )
 
         self.endpoint_state = EndpointState(self.kv_store, self.long_poll_host)
@@ -260,6 +260,8 @@ class ServeController:
         self,
         deployment_id: str,
         handle_id: str,
+        actor_id: Optional[str],
+        lives_on_proxy_or_replica: bool,
         queued_requests: float,
         running_requests: Dict[str, float],
         send_timestamp: float,
@@ -269,7 +271,13 @@ class ServeController:
             f"{queued_requests} queued requests and {running_requests} running requests"
         )
         self.deployment_state_manager.record_handle_metrics(
-            deployment_id, handle_id, queued_requests, running_requests, send_timestamp
+            deployment_id,
+            handle_id,
+            actor_id,
+            lives_on_proxy_or_replica,
+            queued_requests,
+            running_requests,
+            send_timestamp,
         )
 
     def _dump_autoscaling_metrics_for_testing(self):
@@ -430,6 +438,11 @@ class ServeController:
                     )
                 except Exception:
                     logger.exception("Exception updating proxy state.")
+
+            if not any_recovering:
+                self.deployment_state_manager.drop_invalid_handle_metrics(
+                    self.proxy_state_manager.get_alive_proxy_actor_ids()
+                )
 
             loop_duration = time.time() - loop_start_time
             if loop_duration > 10:
--- python/ray/serve/_private/router.py
+++ python/ray/serve/_private/router.py
@@ -50,12 +50,16 @@ class RouterMetricsManager:
         self,
         deployment_id: DeploymentID,
         handle_id: str,
+        self_actor_id: str,
+        lives_on_proxy_or_replica: bool,
         controller_handle: ActorHandle,
         router_requests_counter: metrics.Counter,
         queued_requests_gauge: metrics.Gauge,
     ):
         self._handle_id = handle_id
         self._deployment_id = deployment_id
+        self._self_actor_id = self_actor_id
+        self._lives_on_proxy_or_replica = lives_on_proxy_or_replica
         self._controller_handle = controller_handle
 
         # Exported metrics
@@ -216,7 +220,12 @@ class RouterMetricsManager:
         """
 
         self._controller_handle.record_handle_metrics.remote(
-            **self._get_aggregated_requests(), send_timestamp=time.time()
+            send_timestamp=time.time(),
+            deployment_id=self._deployment_id,
+            handle_id=self._handle_id,
+            actor_id=self._self_actor_id,
+            lives_on_proxy_or_replica=self._lives_on_proxy_or_replica,
+            **self._get_aggregated_requests(),
         )
 
     def _add_autoscaling_metrics_point(self):
@@ -245,8 +254,6 @@ class RouterMetricsManager:
             }
 
         return {
-            "deployment_id": self._deployment_id,
-            "handle_id": self._handle_id,
             "queued_requests": self.num_queued_requests,
             "running_requests": running_requests,
         }
@@ -267,6 +274,7 @@ class Router:
         self_node_id: str,
         self_actor_id: str,
         self_availability_zone: Optional[str],
+        lives_on_proxy_or_replica: bool,
         event_loop: asyncio.BaseEventLoop = None,
         _prefer_local_node_routing: bool = False,
         enable_queue_len_cache: bool = RAY_SERVE_ENABLE_QUEUE_LENGTH_CACHE,
@@ -334,6 +342,8 @@ class Router:
         self._metrics_manager = RouterMetricsManager(
             deployment_id,
             handle_id,
+            self_actor_id,
+            lives_on_proxy_or_replica,
             controller_handle,
             metrics.Counter(
                 "serve_num_router_requests",
