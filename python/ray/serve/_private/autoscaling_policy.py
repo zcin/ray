6f7025cb3dae2bb00b8ac8c8a530c4c4e7c13773
Cindy Zhang <cindyzyx9@gmail.com>
2024-04-16 15:34:14 -0700

    [serve] combine autoscaling state and autoscaling policy manager
    To be written.
    
    
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/serve/_private/autoscaling_policy.py
+++ python/ray/serve/_private/autoscaling_policy.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 from ray.serve._private.common import TargetCapacityDirection
 from ray.serve._private.constants import SERVE_LOGGER_NAME
-from ray.serve._private.utils import get_capacity_adjusted_num_replicas
 from ray.serve.config import AutoscalingConfig
 
 logger = logging.getLogger(SERVE_LOGGER_NAME)
@@ -100,17 +99,3 @@ class AutoscalingPolicyManager:
         target_capacity_direction: Optional[TargetCapacityDirection] = None,
     ) -> int:
         """Clips curr_target_num_replicas using the current bounds."""
-
-        upper_bound = get_capacity_adjusted_num_replicas(
-            self.config.max_replicas,
-            target_capacity,
-        )
-        lower_bound = self.get_current_lower_bound(
-            target_capacity,
-            target_capacity_direction,
-        )
-        return max(lower_bound, min(upper_bound, curr_target_num_replicas))
-
-    def get_metrics_interval_s(self) -> Optional[float]:
-        if self.config:
-            return self.config.metrics_interval_s
