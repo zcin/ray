450198abc23dff3e7155335843ffa58a7e74572f
Cindy Zhang <cindyzyx9@gmail.com>
2024-05-22 09:43:03 -0700

    [runtime_env] change container manager to unified plugin mechanism
    Currently, we create a custom `ContainerManager` class and invoke it specially with `container_manager.setup()`. This doesn't conform with the runtime env plugin pattern we use for every other runtime env field.
    
    This PR removes `ContainerManager` and moves the container code to `ContainerPlugin` which extends the base `RuntimeEnvPlugin` class. It changes the callsite to run `create_plugin_if_needed` instead of `container_manager.setup()`. This way, in the future if we add support for using `container` together with other runtime env fields, the API is more unified.
    
    Signed-off-by: Cindy Zhang <cindyzyx9@gmail.com>

--- python/ray/runtime_env/runtime_env.py
+++ python/ray/runtime_env/runtime_env.py
@@ -288,6 +288,7 @@ class RuntimeEnv(dict):
         "worker_process_setup_hook",
         "_nsight",
         "mpi",
+        "image_uri",
     }
 
     extensions_fields: Set[str] = {
@@ -310,6 +311,7 @@ class RuntimeEnv(dict):
         config: Optional[Union[Dict, RuntimeEnvConfig]] = None,
         _validate: bool = True,
         mpi: Optional[Dict] = None,
+        image_uri: Optional[str] = None,
         **kwargs,
     ):
         super().__init__()
@@ -335,6 +337,8 @@ class RuntimeEnv(dict):
             runtime_env["worker_process_setup_hook"] = worker_process_setup_hook
         if mpi is not None:
             runtime_env["mpi"] = mpi
+        if image_uri is not None:
+            runtime_env["image_uri"] = image_uri
         if runtime_env.get("java_jars"):
             runtime_env["java_jars"] = runtime_env.get("java_jars")
 
